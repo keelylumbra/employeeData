e3cd3201e5b6dac563f74e70038edd79e85df128
Jacob Clark <jtclark@knights.ucf.edu>
2016-08-09 19:21:03 -0400

    deleted old firebase initialize

--- assets/logic.js
+++ assets/logic.js
@@ -1,16 +1,3 @@
-// Initialize Firebase
-// var config = {
-// 	apiKey: "AIzaSyBE4rIfU_He-8pm0yhw6W8ux1lKpQt19h8",
-// 	authDomain: "fir-presence.firebaseapp.com",
-// 	databaseURL: "https://fir-presence.firebaseio.com",
-// 	storageBucket: "fir-presence.appspot.com",
-// };
-// firebase.initializeApp(config);
-
-// // Create a variable to reference the database.
-// var database = firebase.database();
-
-
 
 /* Created a new database just for this app so it doesn't interrupt others **jtc**
 // Initialize Firebase
