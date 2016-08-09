85f36239265f466a7ad9713f872a06dce309e016
Keely <keelylumbra@knights.ucf.edu>
2016-08-09 19:03:59 -0400

    html

---
+++ assets/logic.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+// var config = {
+// 	apiKey: "AIzaSyBE4rIfU_He-8pm0yhw6W8ux1lKpQt19h8",
+// 	authDomain: "fir-presence.firebaseapp.com",
+// 	databaseURL: "https://fir-presence.firebaseio.com",
+// 	storageBucket: "fir-presence.appspot.com",
+// };
+// firebase.initializeApp(config);
+
+// // Create a variable to reference the database.
+// var database = firebase.database();
+
+
+
