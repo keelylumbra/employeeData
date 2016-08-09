3b100c01d53f1061278de81e7cc852161a21668b
Jacob Clark <jtclark@knights.ucf.edu>
2016-08-09 19:16:04 -0400

    updated logic.js for new database just for the employee app

--- assets/logic.js
+++ assets/logic.js
@@ -12,3 +12,14 @@
 
 
 
+/* Created a new database just for this app so it doesn't interrupt others **jtc**
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDPKDNOWQRrOosLLIA4CKPCp00pcxfEsR4",
+    authDomain: "empdata-17990.firebaseapp.com",
+    databaseURL: "https://empdata-17990.firebaseio.com",
+    storageBucket: "empdata-17990.appspot.com",
+};
+firebase.initializeApp(config);
+*/
+
